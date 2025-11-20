04e8f4a1981fe2f16bc2087e02f8b3f45468a032
Timur32 <gajsint411@gmail.com>
2025-11-20 21:54:53 +0500

    modeife gen_keys

--- app/auth/utils/gen_keys.py
+++ app/auth/utils/gen_keys.py
@@ -1,11 +1,19 @@
-# Пример с библиотекой cryptography
-# Установка: pip install cryptography
+import os
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ed25519
 
+# Определяем путь к директории для ключей
+KEY_DIR = "./app/auth/certs"
+
+# 0. Проверка и создание директории, если её нет
+if not os.path.exists(KEY_DIR):
+    os.makedirs(KEY_DIR)
+    print(f"Директория '{KEY_DIR}' была создана.")
+else:
+    print(f"Директория '{KEY_DIR}' уже существует.")
+
 # 1. Генерация ключей
 private_key = ed25519.Ed25519PrivateKey.generate()
-
 public_key = private_key.public_key()
 
 # 2. Сериализация закрытого ключа в PEM-формат и запись в файл
@@ -14,7 +22,7 @@ pem_private = private_key.private_bytes(
     format=serialization.PrivateFormat.PKCS8,
     encryption_algorithm=serialization.NoEncryption()
 )
-with open("./app/auth/certs/private_key.pem", "wb") as f:
+with open(os.path.join(KEY_DIR, "private_key.pem"), "wb") as f:
     f.write(pem_private)
 
 # 3. Сериализация открытого ключа в PEM-формат и запись в файл
@@ -22,7 +30,7 @@ pem_public = public_key.public_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PublicFormat.SubjectPublicKeyInfo
 )
-with open("./app/auth/certs/public_key.pem", "wb") as f:
+with open(os.path.join(KEY_DIR, "public_key.pem"), "wb") as f:
     f.write(pem_public)
 
-print("Ключи успешно записаны в файлы private_key.pem и public_key.pem")
+print(f"Ключи успешно записаны в файлы {KEY_DIR}/private_key.pem и {KEY_DIR}/public_key.pem")
