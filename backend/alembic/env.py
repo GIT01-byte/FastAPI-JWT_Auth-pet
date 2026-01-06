4ffc6a4fcba56f6b4810de55a7720cc37ec33e79
Timur33 <gajsint411@gmail.com>
2026-01-07 02:39:35 +0500

    update alembic .env

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -13,7 +13,7 @@ from alembic import context
 
 from auth.core.models import Base
 from auth.core.settings import settings
-print(f'Metadata keys: {Base.metadata.tables.keys()}')
+print(f'Metadata models keys: {Base.metadata.tables.keys()}')
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
