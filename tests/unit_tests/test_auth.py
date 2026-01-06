2728867784655929d4b1c39a8541a8654d6b9b65
Timur33 <gajsint411@gmail.com>
2026-01-07 02:29:09 +0500

    update .env.example

--- tests/unit_tests/test_auth.py
+++ tests/unit_tests/test_auth.py
@@ -15,11 +15,29 @@ class TestTokens:
         assert isinstance(hashed_token, str)
 
 class TestApi:
+    # TODO: ОТЛАДИТЬ ТЕСТ:
+    # FAILED tests/unit_tests/test_auth.py::TestApi::test_unauthorize_user[/users/login/-POST-401] - assert 422 == 401
+    # FAILED tests/unit_tests/test_auth.py::TestApi::test_unauthorize_user[/users/logout-POST-401] - assert 307 == 401
+    # FAILED tests/unit_tests/test_auth.py::TestApi::test_unauthorize_user[/users/me-GET-401] - assert 307 == 401
     @pytest.mark.asyncio
-    async def test_unauthorize_user(self):
+    @pytest.mark.parametrize(
+        "url, method, st_code",
+        [
+            ["/users/login/", "POST", 401],
+            # ["/users/tokens/refresh/", 401],
+            ["/users/logout", "POST", 401],
+            ["/users/me", "GET", 401],
+        ]
+    )
+    async def test_unauthorize_user(self, url, method, st_code):
         async with AsyncClient(
             transport=ASGITransport(app=app),
             base_url="http://127.0.0.1:8080"
         ) as ac:
-            response = await ac.get("/users/me/")
-            assert response.status_code == 401
+            if method == "GET":
+                response = await ac.get(url=url)
+                assert response.status_code == st_code
+            elif method == "POST":
+                response = await ac.post(url=url)
+                assert response.status_code == st_code
+    
